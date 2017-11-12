8b13246a692521b1b9dcc0e3cfc986f044794acd
Julian Selser <julianselser@yahoo.com.ar>
2017-11-12 17:05:52 -0300

    - Ahora se pueden configurar los nodos via archivo - Manejo capacidades de clave/valor y capacidad segun enunciado

--- Nodo.js
+++ Nodo.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const resolve = require("path").resolve;
 
 //API REST
 let express = require('express');
@@ -8,12 +9,30 @@ app.use(bodyParser.json());
 
 //Nodo
 let NodoWorker = require('./modules/NodoWorker');
+let config = require('./config'); //default
 
-if(process.argv.length !== 3) {
-	console.error('Faltan parámetros: node Nodo.js <puerto>');
+if(process.argv.length < 3) {
+	console.error('Faltan parámetros: node Nodo.js <puerto> [config]');
 	process.exit();
 }
 
+//vemos si el usuario paso
+//un archivo de configuracion
+let userConfig = process.argv[3];
+if(userConfig) {
+	try {
+		config = require(resolve(userConfig));
+	} catch(e) {
+		console.error('No se puedo cargar el archivo de configuracion: ' + userConfig);
+		process.exit();
+	}
+}
+
+//chequeamos que la configuracion tenga lo que necesitamos
+if(!config.maxKeySize || !config.maxPairNum || !config.maxValSize) {
+	console.error('Los campos maxKeySize, maxPairNum, maxValSize son obligatorios en la configuracion')
+}
+
 let port = process.argv[2];
-NodoWorker.init(app, port);
-console.log('Nodo iniciado en puerto '+port);
+NodoWorker.init(app, port, config);
+console.log('Nodo iniciado en puerto ' + port + ' usando la configuracion de ' + (userConfig || 'config.json'));
--- modules/NodoWorker.js
+++ modules/NodoWorker.js
@@ -1,64 +1,84 @@
 let debug = require('debug')('KVStore:NodoWorker');
 
 let NodoWorker = {
-	
+
 	data: {},
-	
-	init: function(app, port) {
-		
+
+	init: function(app, port, config) {
+        NodoWorker.config = config;
 		NodoWorker.initServer(app, port);
-	
 	},
-	
+
 	initServer: function(app, port) {
-		
+
 		app.get('/status', function(request, response) {
 			response.send({ success: true });
 		});
-	
+
 		app.get('/:key', function(request, response) {
 			NodoWorker.get(response, request.params.key);
 		});
-	
+
 		app.post('/', function(request, response) {
-			if(!request.body.key || !request.body.value) {
-				NodoWorker.error(response, 'JSON inválido');
-			} else {
-				NodoWorker.save(response, request.body.key, request.body.value);
-			}
-		});
+			const error = NodoWorker.checkErrors(request);
+
+            if(error) {
+              NodoWorker.error(response, error.errorStatus, error.errorMessage);
+            } else {
+			  NodoWorker.save(response, request.body.key, request.body.value);
+            }
+        });
 
 		app.listen(port);
-		
 	},
-	
-	error: function(response, msg) {
-		response.status(404).send({ success: false, error: msg });
+
+	checkErrors: function (request) {
+        const config = NodoWorker.config;
+        const exceedsBytes = (str, megabytes) => Buffer.byteLength(key, 'utf8') > megabytes * Math.pow(2, 20);
+      
+        const key = request.body.key;
+        const value = request.body.value;
+
+		if(!key || !value)
+			return {errorStatus: 404, errorMessage: 'JSON inválido'};
+
+        if(typeof key !== 'string' || typeof value !== 'string')
+            return {errorStatus: 400, errorMessage: 'La clave y el valor deben ser strings'};
+
+        if(Object.keys(NodoWorker.data).length + 1 > config.maxPairNum)
+            return {errorStatus: 400, errorMessage: 'El nodo no puede aceptar mas datos, se alcanzaron los ' + config.maxPairNum};
+      
+        if(exceedsBytes(key, config.maxKeySize) || exceedsBytes(value, config.maxValSize))
+            return {errorStatus: 400, errorMessage: 'La clave o el valor exceden el tamaño indicado en la configuracion'};
 	},
-	
+
+	error: function(response, status, msg) {
+		response.status(status).send({ success: false, error: msg });
+	},
+
 	save: function(response, key, value) {
-		
+
 		debug('Almacenando clave %s', key);
-		
+
 		NodoWorker.data[key] = value;
 		response.send({ success: true, key: key, value: value });
-		
+
 	},
-	
+
 	get: function(response, key) {
-		
+
 		if(typeof NodoWorker.data[key] !== 'undefined') {
 			let value = NodoWorker.data[key];
-			
+
 			debug('Leyendo clave %s', key);
 			response.send({ success: true, key: key, value: value });
 		} else {
 			debug('Clave %s inexistente', key);
-			NodoWorker.error(response, 'Clave inexistente');
+			NodoWorker.error(response, 404, 'Clave inexistente');
 		}
-		
+
 	}
-	
+
 };
 
 module.exports = NodoWorker;
